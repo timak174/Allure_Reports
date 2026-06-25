92776e4a93c29a4502836f3e6008234087b2251a
timak09.06 <timak_vaa@mail.ru>
2026-06-25 17:54:18 +0500

    Initial commit

---
+++ src/test/java/qa/test/allure/SelenideTest.java
@@ -0,0 +1,33 @@
+package qa.test.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+    @BeforeAll
+    static void setupAllureReports() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
+    }
+    @Test
+    public void testIssueSearch() {
+        open("https://github.com");
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#_r_6_--label");
+        $(withText("#1")).should(exist);
+    }
+}
